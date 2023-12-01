8985c081eafae1d7a4fcaa0eff2448174ed4f087
Timon Heuwekemeijer <timon.heuwekemeijer@secura.com>
2023-12-01 11:48:29 +0100

    progress

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input/example.txt").unwrap();
+    let lines = input.lines();
+    let mut sum = 0;
+    for line in lines {
+        sum += get_value(line);
+    }
+}
+
+fn get_value(line: &str) -> i32 {
+    0
 }
