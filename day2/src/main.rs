f794cba09fcbc22b62c8c938dd6d3f80202c3813
Timonheu <timonheu@gmail.com>
2023-12-02 11:02:54 +0100

    progress towards part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+
+struct Maximum {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let maximum = Maximum {
+        red: 11,
+        green: 12,
+        blue: 13,
+    };
+
+    let input = fs::read_to_string("input/example.txt").unwrap();
+    let lines = input.lines();
+
+    let sum = 0;
+
+    for line in lines {
+        let id_split = line.split(':');
+        let id_strip = id_split.last().unwrap();
+        let rounds = id_strip.split(';');
+        for round in rounds {
+            let color_split = round.split(',');
+            for color_line in color_split {
+                let mut split = color_line.split(' ');
+                let amount: i32 = split.next().unwrap().parse().unwrap();
+                let color = split.next().unwrap();
+                match color {
+                    _ => panic!("invalid color {color}"),
+                }
+            }
+        }
+    }
 }
