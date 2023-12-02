78263d8f5caa40bb07693e47f222939b57138cc7
Timonheu <timonheu@gmail.com>
2023-12-02 11:15:42 +0100

    gives an answer that is way too high

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -16,22 +16,45 @@ fn main() {
     let input = fs::read_to_string("input/example.txt").unwrap();
     let lines = input.lines();
 
-    let sum = 0;
+    let mut sum = 0;
+    let mut id = 1;
 
     for line in lines {
+        let mut valid = true;
         let id_split = line.split(':');
         let id_strip = id_split.last().unwrap();
         let rounds = id_strip.split(';');
         for round in rounds {
             let color_split = round.split(',');
             for color_line in color_split {
-                let mut split = color_line.split(' ');
+                let mut split = color_line.trim().split(' ');
+
                 let amount: i32 = split.next().unwrap().parse().unwrap();
                 let color = split.next().unwrap();
                 match color {
+                    "red" => {
+                        if amount >= maximum.red {
+                            valid = false;
+                        }
+                    }
+                    "green" => {
+                        if amount >= maximum.green {
+                            valid = false;
+                        }
+                    }
+                    "blue" => {
+                        if amount >= maximum.blue {
+                            valid = false;
+                        }
+                    }
                     _ => panic!("invalid color {color}"),
                 }
             }
         }
+        if valid {
+            sum += id;
+        }
+        id += 1;
     }
+    println!("Answer to part 1 is {sum}.");
 }
