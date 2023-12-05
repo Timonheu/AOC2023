19ab12949f852670d9cc00ffc8bf4a41cebde29a
Timon Heuwekemeijer <timon.heuwekemeijer@secura.com>
2023-12-05 16:06:29 +0100

    solved part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,17 +1,19 @@
+use std::fs;
+
 #[derive(Debug)]
 struct Range {
-    start: i32,
-    end: i32,
-    target: i32,
+    start: i64,
+    end: i64,
+    target: i64,
 }
 
 impl Range {
-    fn contains(&self, input: i32) -> bool {
+    fn contains(&self, input: i64) -> bool {
         input >= self.start && input <= self.end
     }
 
     // assumes input is in this range
-    fn convert(&self, input: i32) -> i32 {
+    fn convert(&self, input: i64) -> i64 {
         assert!(
             self.contains(input),
             "Input {input} is not in range {:?}",
@@ -26,7 +28,7 @@ struct Conversion {
 }
 
 impl Conversion {
-    fn convert(&self, input: i32) -> i32 {
+    fn convert(&self, input: i64) -> i64 {
         for range in &self.ranges {
             if range.contains(input) {
                 return range.convert(input);
@@ -36,4 +38,56 @@ impl Conversion {
     }
 }
 
-fn main() {}
+fn main() {
+    let input = fs::read_to_string("input/input.txt").unwrap();
+    let mut lines = input.lines();
+
+    let seeds: Vec<i64> = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .last()
+        .unwrap()
+        .trim()
+        .split(' ')
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    // Some ugly ad-hoc parsing
+    assert!(lines.next().unwrap().is_empty());
+    let mut conversions: Vec<Conversion> = vec![];
+    for _i in 0..7 {
+        // conversion title
+        lines.next();
+        let mut range_line = lines.next().unwrap();
+        let mut conversion = Conversion { ranges: vec![] };
+        while !range_line.is_empty() {
+            let mut split = range_line.split(' ');
+            let target: i64 = split.next().unwrap().parse().unwrap();
+            let start: i64 = split.next().unwrap().parse().unwrap();
+            let range: i64 = split.next().unwrap().parse().unwrap();
+            let end = start + range;
+            conversion.ranges.push(Range { start, end, target });
+            let next = lines.next();
+            if next.is_some() {
+                range_line = next.unwrap();
+            } else {
+                break;
+            }
+        }
+        conversions.push(conversion);
+    }
+
+    let mut minimum_location_number: i64 = i64::MAX;
+
+    for seed in seeds {
+        let mut value = seed;
+        for conversion in &conversions {
+            value = conversion.convert(value);
+        }
+        if value < minimum_location_number {
+            minimum_location_number = value;
+        }
+    }
+    println!("Answer to part 1: {minimum_location_number}");
+}
