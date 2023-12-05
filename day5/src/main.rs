772922de9edd9334abf5dfecbd32e4a70b08f04e
Timonheu <timonheu@gmail.com>
2023-12-05 20:45:34 +0100

    getting rid of this solution but committing so I can revert if needed

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -41,11 +41,7 @@ impl Conversion {
     fn get_range(&self, input: i64) -> Option<Range> {
         for range in &self.ranges {
             if range.contains(input) {
-                return Some(Range {
-                    start: range.start,
-                    end: range.end,
-                    target: range.target,
-                });
+                return Some(range.clone());
             }
         }
         None
@@ -105,16 +101,63 @@ fn main() {
     }
     println!("Answer to part 1: {minimum_location_number}.");
 
-    //find the optimal range from the ground up
-    let minimal_range = conversions.last().unwrap().ranges[0].clone();
-    let minimal_start_end = (minimal_range.start, minimal_range.end);
+    // Time to optimize: collapse all conversions into one
+    let mut result: Vec<Range> = vec![];
+    let mut i: usize = 0;
+    while i < conversions.len() - 1 {
+        let conversion = conversions[i].clone();
+        for range in conversion.ranges.clone() {
+            let max = range.end - range.start;
+            let converted_start = conversion.convert(range.start);
+            let converted_end = conversion.convert(range.end);
+            //println!("{max}");
+            let mut j = 0;
+            while j <= max {
+                let current_input = converted_start + j;
+                let current_value = conversion.convert(current_input);
 
-    let mut reverse_conversions = conversions.clone();
-    reverse_conversions.reverse();
-
-    for conversion in reverse_conversions {
-        for range in conversion.ranges {}
+                //println!("Current input: {current_input}, Current value: {current_value}");
+                if current_value != current_input {
+                    let target_range = conversion.get_range(current_value).unwrap();
+                    //println!("target range: {:?}", target_range);
+                    //if the target range can fit the rest of this remaining range
+                    if target_range.end - current_value >= converted_end - current_input {
+                        result.push(Range {
+                            start: current_value,
+                            end: conversion.convert(converted_end),
+                            target: conversions[i + 1].convert(current_value),
+                        });
+                        break;
+                    } else {
+                        // target range can not fit the entirety of this remaining range
+                        result.push(Range {
+                            start: current_value,
+                            end: target_range.end,
+                            target: conversions[i + 1].convert(current_value),
+                        });
+                        //assert!(                      current_value < target_range.end,                            "Target range end: {}, Current value: {current_value}",                            target_range.end                        );
+                        j += target_range.end - current_value + 1;
+                        //print!("j: {j}");
+                    }
+                } else {
+                    j += 1;
+                }
+            }
+        }
+        i += 1;
     }
 
-    println!("Answer to part 2: {minimum_location_number}.");
+    let single_conversion = Conversion { ranges: result };
+
+    let mut minimum_location_number: i64 = i64::MAX;
+
+    for seed in seeds.clone() {
+        let mut value = seed;
+        value = single_conversion.convert(value);
+        if value < minimum_location_number {
+            minimum_location_number = value;
+        }
+    }
+
+    println!("Answer to part 1: {minimum_location_number}.");
 }
