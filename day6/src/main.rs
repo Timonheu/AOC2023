72c9f6e91ed6a2d9603e288d90500618f9e1044a
Timon Heuwekemeijer <timon.heuwekemeijer@secura.com>
2023-12-07 16:28:47 +0100

    initial parsing

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input/example.txt").unwrap();
+    let mut lines = input.lines();
+    let times: Vec<i32> = lines
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|s| !s.eq_ignore_ascii_case("Time:"))
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let distances: Vec<i32> = lines
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|s| !s.eq_ignore_ascii_case("Distance:"))
+        .map(|x| x.parse().unwrap())
+        .collect();
 }
